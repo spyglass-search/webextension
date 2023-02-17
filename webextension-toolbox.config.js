6c661b5ddccf976f8f348463c995ee5c20efb9b6
Andrew Huynh <andrew@spyglass.fyi>
2023-02-16 17:46:13 -0800

    adding deps for react conversion

--- webextension-toolbox.config.js
+++ webextension-toolbox.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    webpack: (config, { dev, vendor }) => {
-        config.module.rules.push({
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader", "postcss-loader"],
-        });
+  webpack: (config, { dev, vendor }) => {
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: ["style-loader", "css-loader", "postcss-loader"],
+    });
 
-        return config;
-    },
+    return config;
+  },
 };
