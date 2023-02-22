f610bf711a95194774df95f4aaf416d76c50affa
Andrew Huynh <andrew@spyglass.fyi>
2023-02-21 16:38:18 -0800

    keep firefox on manifest v2 until they're ready

--- app/scripts/lib/utils.ts
+++ app/scripts/lib/utils.ts
@@ -1,28 +1,29 @@
 import browser from "webextension-polyfill";
+let action = browser.action || browser.browserAction;
 
 export function mark_indexed() {
-  browser.action.setBadgeText({ text: "✓" });
-  browser.action.setBadgeBackgroundColor({ color: "rgb(21, 128, 61)" });
-  browser.action.setTitle({ title: "This URL is in your library" });
+  action.setBadgeText({ text: "✓" });
+  action.setBadgeBackgroundColor({ color: "rgb(21, 128, 61)" });
+  action.setTitle({ title: "This URL is in your library" });
 
-  if (process.env.VENDOR == "firefox") {
-    browser.action.setBadgeTextColor({ color: "rgb(255, 255, 255)" });
+  if (action.setBadgeTextColor) {
+    action.setBadgeTextColor({ color: "rgb(255, 255, 255)" });
   }
 }
 
 export function mark_unindexed() {
-  browser.action.setBadgeText({ text: "" });
-  browser.action.setBadgeBackgroundColor({ color: "rgb(0, 1, 1)" });
-  browser.action.setTitle({ title: "Click to add to your library!" });
+  action.setBadgeText({ text: "" });
+  action.setBadgeBackgroundColor({ color: "rgb(0, 1, 1)" });
+  action.setTitle({ title: "Click to add to your library!" });
 }
 
 export function mark_error(title: string) {
-  browser.action.setBadgeText({ text: "⚠" });
-  browser.action.setBadgeTextColor({ color: "rgb(255, 255, 255)" });
-  browser.action.setBadgeBackgroundColor({ color: "rgb(256, 0, 0)" });
-  browser.action.setTitle({ title });
+  action.setBadgeText({ text: "⚠" });
+  action.setBadgeTextColor({ color: "rgb(255, 255, 255)" });
+  action.setBadgeBackgroundColor({ color: "rgb(256, 0, 0)" });
+  action.setTitle({ title });
 
-  if (process.env.VENDOR == "firefox") {
-    browser.action.setBadgeTextColor({ color: "rgb(255, 255, 255)" });
+  if (action.setBadgeTextColor) {
+    action.setBadgeTextColor({ color: "rgb(255, 255, 255)" });
   }
 }
